f70c51641c4443078cd9cb1252bb36717556e20f
jjsskk <akrn112@naver.com>
2024-05-17 23:39:52 +0900

    백준/뱀 문제 추가

---
+++ 백준/빡구현/뱀.cc
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, k, l;
+int answer = 1;
+queue<pair<int, int>> q; // 뱀의 몸 (y,x)
+int m[101][101] = {
+    0,
+}; // 0 : 빈칸 1: 사과
+int s[101];
+char d[101];
+// int y = 0, x = 0;               // 머리의 위치
+const int dy[] = {0, 1, 0, -1}; // 동남서북 ( 시계방향)
+const int dx[] = {1, 0, -1, 0}; // 동남서북
+int cur_dir = 0;
+bool check(int cur_y, int cur_x)
+{
+    for (int i = 0; i < q.size(); i++)
+    {
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
+        if (cur_y == y && cur_x == x)
+            return false;
+        q.push({y, x});
+    }
+    return true;
+}
+void eat(int y, int x)
+{
+    q.push({y, x});
+    if (m[y][x] == 1)
+    {
+        m[y][x] = 0;
+        return;
+    }
+    q.pop();
+}
+void play()
+{
+    q.push({0, 0});
+    for (int i = 0; i < l; i++)
+    {
+        int cur_x = q.back().second;
+        int cur_y = q.back().first;
+        while (answer <= s[i] || i == l - 1)
+        {
+            int ny = cur_y + dy[cur_dir];
+            int nx = cur_x + dx[cur_dir];
+            if (ny < 0 || nx < 0 || nx >= n || ny >= n)
+                return;
+            if (!check(ny, nx))
+                return;
+            cur_x = nx;
+            cur_y = ny;
+            answer++;
+            eat(ny, nx);
+        }
+
+        if (d[i] == 'L' && answer == s[i] + 1)
+        {
+            cur_dir--;
+            if (cur_dir == -1)
+                cur_dir = 3;
+        }
+
+        if (d[i] == 'D' && answer ==  s[i] + 1)
+        {
+            cur_dir++;
+            if (cur_dir == 4)
+                cur_dir = 0;
+        }
+    }
+}
+int main()
+{
+    cin >> n >> k;
+    int a, h;
+    for (int i = 0; i < k; i++)
+    {
+        cin >> a >> h;
+        m[a-1][h-1] = 1;
+    }
+
+    cin >> l;
+    int b;
+    char c;
+    for (int i = 0; i < l; i++)
+    {
+        cin >> b >> c;
+        s[i] = b;
+        d[i] = c;
+    }
+
+    play();
+    cout << answer;
+    return 0;
+}
