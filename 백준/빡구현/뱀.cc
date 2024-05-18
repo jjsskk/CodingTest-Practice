c77401ed16cf38b20a341baf19151044191473b2
jjsskk <akrn112@naver.com>
2024-05-18 21:44:08 +0900

    백준/뱀 문제 수정

--- 백준/빡구현/뱀.cc
+++ 백준/빡구현/뱀.cc
@@ -9,13 +9,13 @@ int m[101][101] = {
 }; // 0 : 빈칸 1: 사과
 int s[101];
 char d[101];
-// int y = 0, x = 0;               // 머리의 위치
 const int dy[] = {0, 1, 0, -1}; // 동남서북 ( 시계방향)
 const int dx[] = {1, 0, -1, 0}; // 동남서북
 int cur_dir = 0;
 bool check(int cur_y, int cur_x)
 {
-    for (int i = 0; i < q.size(); i++)
+    int size = (int)q.size();
+    for (int i = 0; i < size; i++)
     {
         int y = q.front().first;
         int x = q.front().second;
@@ -43,7 +43,7 @@ void play()
     {
         int cur_x = q.back().second;
         int cur_y = q.back().first;
-        while (answer <= s[i] || i == l - 1)
+        while (answer <= s[i] || i == l - 1) // 모든 명령 끝난후 뱀이 벽에 부딪힐떄까지 움직이도록 해야함
         {
             int ny = cur_y + dy[cur_dir];
             int nx = cur_x + dx[cur_dir];
@@ -55,20 +55,20 @@ void play()
             cur_y = ny;
             answer++;
             eat(ny, nx);
-        }
 
-        if (d[i] == 'L' && answer == s[i] + 1)
-        {
-            cur_dir--;
-            if (cur_dir == -1)
-                cur_dir = 3;
-        }
-
-        if (d[i] == 'D' && answer ==  s[i] + 1)
-        {
-            cur_dir++;
-            if (cur_dir == 4)
-                cur_dir = 0;
+            if (d[i] == 'L' && answer == s[i] + 1)
+            // 가장 마지막 명령에서 while문을 빠져나가지 못하기 떄문에 방향전환 기능은 무조건 while문 안에 있어야함
+            {
+                cur_dir--;
+                if (cur_dir == -1)
+                    cur_dir = 3;
+            }
+            else if (d[i] == 'D' && answer == s[i] + 1)
+            {
+                cur_dir++;
+                if (cur_dir == 4)
+                    cur_dir = 0;
+            }
         }
     }
 }
@@ -79,7 +79,7 @@ int main()
     for (int i = 0; i < k; i++)
     {
         cin >> a >> h;
-        m[a-1][h-1] = 1;
+        m[a - 1][h - 1] = 1;
     }
 
     cin >> l;
